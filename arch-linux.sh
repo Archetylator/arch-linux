c52b91eda876eed82228e8aef2bc730c9d3fd07e
Marcin Syngajewski <Archetylator@users.noreply.github.com>
2016-08-27 17:59:04 +0300

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -19,8 +19,8 @@ if [ `ls --almost-all /sys/firmware/efi/efivars` ]
 then
   echo -e "UEFI system \e[0;32;47m [OK] \e[0m 0;32m \t"
 else
-	echo -e "Script is designed to work only with UEFI system" 1>&2
-	exit 1
+  echo -e "Script is designed to work only with UEFI system" 1>&2
+  exit 1
 fi
 
 # enable network time synchronization
