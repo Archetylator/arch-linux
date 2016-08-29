fd5968fc399b025b36ade39c4779681b4c459241
Marcin Syngajewski <Archetylator@users.noreply.github.com>
2016-08-29 23:19:05 +0300

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -36,15 +36,14 @@ timedatectl set-ntp true
 # set polish keyboard layout
 loadkeys pl
 
-echo "Enter device (eg. /dev/sda):"
-read DEVICE
+read -e -p "Enter device (eg. /dev/sda):" -i "/dev/sda" DEVICE
 
 parted $DEVICE --script mkpart primary fat32 0% 512MiB
-parted $DEVICE --script mklabel gpt 
+parted $DEVICE --script mklabel 1 gpt 
 parted $DEVICE --script set 1 boot on 
 parted $DEVICE --script name 1 "EFP with systemd-boot"
 
 parted $DEVICE --script mkpart primary 512MiB 100% 
-parted $DEVICE --script mklabel gpt 
+parted $DEVICE --script mklabel 2 gpt 
 parted $DEVICE --script set 2 LVM on 
 parted $DEVICE --script name 2 "Arch LVM on LUKS"
