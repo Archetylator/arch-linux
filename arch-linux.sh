413770e3f296cfd10f12c8643171cdc3219d2c1a
Marcin Syngajewski <Archetylator@users.noreply.github.com>
2016-08-30 11:44:22 +0300

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -48,3 +48,61 @@ parted $DEVICE --script name 1 'EFP with systemd-boot'
 parted $DEVICE --script mkpart primary 512MiB 100% 
 parted $DEVICE --script set 2 LVM on 
 parted $DEVICE --script name 2 'Arch LVM on LUKS'
+
+PARTITION1=$DEVICE"1"
+PARTITION2=$DEVICE"2"
+
+mkfs.vfat -F32 $PARTITION1
+
+cryptsetup --cipher aes-xts-plain64 --verify-passphrase --use-random luksFormat $PARTITION2
+cryptsetup luksOpen $PARTITION2 luks
+
+pvcreate /dev/mapper/luks
+vgcreate arch /dev/mapper/luks
+
+read -e -p "Enter size for swap partition:" -i "8G" SWAPSIZE
+lvcreate --size $SWAPSIZE arch --name swap
+
+read -e -p "Enter size for home partition:" -i "50G" HOMESIZE
+lvcreate --size $HOMESIZE arch --name home
+
+lvcreate -l +100%FREE arch --name root
+
+mkfs.ext4 /dev/mapper/arch-root
+mkfs.ext4 /dev/mapper/arch-home
+mkswap /dev/mapper/arch-swap
+
+mount /dev/mapper/arch-root /mnt
+
+swapon /dev/mapper/arch-swap
+
+mkdir /mnt/home
+mount /dev/mapper/arch-home /mnt/home
+
+mkdir /mnt/boot
+mkdir /mnt/boot/EFI
+mount $PARTITION1 /mnt/boot
+
+pacstrap /mnt base base-devel vim git efibootmgr dialog wpa_supplicant
+
+genfstab -pU /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt /bin/bash
+
+ln -s /usr/share/zoneinfo/Europe/Sofia /etc/localtime
+hwclock --systohc --utc
+
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo LANGUAGE=en_US >> /etc/locale.conf
+echo LC_ALL=C >> /etc/locale.conf
+
+echo zalman > /etc/hostname
+
+passwd
+
+sed -i 's/\bMODULES="\b/&ext4 /' /etc/mkinitcpio.conf
+sed -i 's/\bHOOKS="\b/&encrypt lvm2 /' /etc/mkinitcpio.conf
+
+mkinitcpio -p linux
+
+bootctl --path=$PARTITION1 install
