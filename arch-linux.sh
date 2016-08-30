37e4b3c1bba217d59de623a91add58c9fb6c007d
Marcin Syngajewski <Archetylator@users.noreply.github.com>
2016-08-30 12:42:55 +0300

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -63,7 +63,7 @@ mkfs.vfat -F32 $PARTITION1
 ok "Set fat32 on EFP partition"
 
 read -e -p "Enter password for LUKS:" PASSWORD
-(echo YES; echo $PASSWORD; echo $PASSWORD) | cryptsetup --cipher aes-xts-plain64 --verify-passphrase --use-random luksFormat $PARTITION2
+(echo YES; echo $PASSWORD; echo $PASSWORD) | cryptsetup --cipher aes-xts-plain64 --use-random luksFormat $PARTITION2
 ok "Encrypted 'LVM on LUKS' partition"
 
 cryptsetup luksOpen $PARTITION2 luks
